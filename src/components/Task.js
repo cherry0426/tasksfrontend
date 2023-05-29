c5cccbe232d27e164cf0a732533701b31b93cbd7
cherry0426 <ananya.katpally26@gmail.com>
2023-05-28 20:08:16 -0500

    frontend integration

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,26 +1,80 @@
-import * as React from 'react';
-import Box from '@material-ui/core/Box';
+import React,{useState, useEffect} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
+import { Container, Paper} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+     
+    },
+  },
+}));
 
 export default function Task() {
+  const paperStyle = {padding:'50px 50px', width:600, margin:'10px auto'}
+  const classes = useStyles();
+  const[task_description,setName]=useState('')
+  const[created_date,setDate]=useState('')
+  const[tasks,setTasks]=useState([])
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const task={task_description  ,created_date}
+    console.log(task)
+    fetch("http://localhost:8080/activity/add",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(task)
+
+  }).then(()=>{
+    console.log("New Task added")
+  })
+}
+
+useEffect(()=>{
+  fetch("http://localhost:8080/activity/add")
+  .then(res=>res.json())
+  .then((result)=>{
+    setTasks(result);
+  }
+)
+},[])
+
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25pc' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-    <div>
-      <TextField id="standard-basic" label="Add Task" variant="outlined" fullwidth/>
-      <TextField id="date" label="" type='datetime-local' variant="outlined"/>
-    </div>
-      <Button color="primary" variant="contained" endIcon={<SendIcon />}>
+
+    <Container>
+    <Paper elevation={3} style ={paperStyle}>  
+     <form className={classes.root} noValidate autoComplete="off">
+      <h1 style={{color:"blue"}}>Add Task</h1>
+      <TextField id="standard-basic" label="Add Task" variant="outlined" fullWidth
+      value={task_description}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="date" label="" type='datetime-local' variant="outlined" fullWidth
+      value={created_date}
+      onChange={(e)=>setDate(e.target.value)}/>
+      <div>
+      <Button color="primary" variant="contained" endIcon={<SendIcon />} onClick={handleClick}>
         Add
-     </Button>
-    </Box>
+     </Button> 
+     </div>
+     </form>
+     {task_description}
+    </Paper>
+
+    <h1 style={{color:"blue"}}>Tasks</h1>
+    <Paper elevation={3} style={paperStyle}>
+      {tasks.map(task=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={task.id}>
+         Task:{task.task_description}<br/>
+         Deadline:{task.created_date}
+        </Paper>
+      ))
+}
+    </Paper>
+    </Container>
   );
 }
